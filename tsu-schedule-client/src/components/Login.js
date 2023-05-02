c505a61e817708c2882ac11329e6726370f2c9c2
Nemetsky Gleb <glebdvp@gmail.com>
2023-05-02 11:48:16 +0700

    basic login and schedule pages on react

---
+++ tsu-schedule-client/src/components/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router';
+import { Alert, Button, Card, Form } from 'react-bootstrap';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'admin' && password === 'password') {
+      // processing authorization
+      navigate('/schedule');
+    } else {
+      setError('Неверное имя пользователя или пароль');
+    }
+  };
+
+  return (
+    <Card className="w-50 mx-auto mt-5">
+      <Card.Body>
+        <h2 className="text-center mb-4">Log In</h2>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Имя</Form.Label>
+            <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)} />
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Пароль</Form.Label>
+            <Form.Control type="password" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          </Form.Group>
+
+          <Button variant="primary" type="submit" className="w-100 mt-4">
+            Log In
+          </Button>
+        </Form>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Login;
---
+++ tsu-schedule-client/src/components/scheduleTable.js
@@ -0,0 +1,92 @@
+// ScheduleTable.jsx
+import React, { useState } from 'react';
+import { Container, Row, Col, Table, Button, Modal } from 'react-bootstrap';
+import scheduleData from './scheduleData';
+
+const ScheduleTable = () => {
+  // Хук для отоборажения окошка с дополнительной информацией о паре
+  const [show, setShow] = useState(false);
+  const [lesson, setLesson] = useState(null);
+
+  const handleClose = () => setShow(false);
+
+  const handleShow = (lesson) => {
+    setLesson(lesson);
+    setShow(true);
+  };
+
+  // Время начала и конца пар
+  const timeMarks = [
+    { startTime: '9:00 AM', endTime: '11:00 AM' },
+    { startTime: '11:00 AM', endTime: '12:00 PM' }
+  ];
+
+  // Цвет пары в таблице
+  const typeColors = {
+    lecture: 'danger',
+    practice: 'primary',
+    meeting: 'warning'
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <h1>Weekly Schedule</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+              </tr>
+            </thead>
+            <tbody>
+              {timeMarks.map((timeMark) => (
+                <tr key={timeMark.startTime}>
+                  <td>{`${timeMark.startTime} - ${timeMark.endTime}`}</td>
+                  {scheduleData.map((day) => {
+                    const lesson = day.lessons.find((l) => l.startTime === timeMark.startTime);
+                    return (
+                      <td key={day.day + timeMark.startTime}>
+                        {lesson && (
+                          <Button variant={typeColors[lesson.type]} onClick={() => handleShow(lesson)}>
+                            {lesson.title}
+                          </Button>
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+      {/* Modal for displaying lesson info */}
+      {lesson && (
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{lesson.title}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Type: {lesson.type}</p>
+            <p>Time: {lesson.startTime} - {lesson.endTime}</p>
+            <p>Additional info: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed hendrerit purus eget urna eleifend tristique. In ac sapien a mi hendrerit tincidunt at ut dolor. Donec eu ultrices metus. Nulla sed turpis ut ipsum euismod egestas vel vel elit.</p>
+          </Modal.Body>
+        </Modal>
+      )}
+    </Container>
+  );
+};
+
+export default ScheduleTable;
