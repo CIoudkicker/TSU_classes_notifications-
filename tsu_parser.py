8041339b0754b827b1e15a7880e658ca36a05864
CIoudkicker <zuev.shura@list.ru>
2023-05-16 23:21:46 +0700

    refactor(tsu_parser): собранны все занятия по дням, по которым стоит отправлять уведомление
    Из полученных данных с апи intime.tsu.ru взяты необходимые краткие
    данные для отправки уведомлений и сохранены в JSON

--- tsu_parser.py
+++ tsu_parser.py
@@ -1,8 +1,9 @@
 import requests
-from typing import Any, Optional
+from typing import Any, Optional, List, Dict
 import datetime
 import json
 from transliterate import translit
+from datetime import datetime as dt
 
 # JSON всех факультетов
 URL_FACULTIES = "https://intime.tsu.ru/api/web/v1/faculties"
@@ -14,7 +15,7 @@ URL_SCHEDULE = "https://intime.tsu.ru/api/web/v1/schedule/group"
 # HTTP запрос к TSU API
 class HttpRequest:
     # Отправка запроса
-    def send_request(self, method: str, url: str, params=None) -> list[dict[str, Any]]:
+    def send_request(self, method: str, url: str, params=None) -> List[Dict[str, Any]]:
         response = requests.request(
             method=method,
             url=url,
@@ -23,7 +24,7 @@ class HttpRequest:
         return self.__check_response(response)
 
     # Проверка запроса
-    def __check_response(self, response: requests.Response) -> Optional[list[dict[str, Any]]]:
+    def __check_response(self, response: requests.Response) -> Optional[List[Dict[str, Any]]]:
         if response.status_code == 200:
             return response.json()
         else:
@@ -58,6 +59,8 @@ class ScheduleExtractor:
         self.date_obj = Date(self.date)
         self.faculty = faculty
         self.group_name = group_name
+        self.schedule_list = list()
+        self.pure_schedule_list = dict()
 
     # Ссылка на список всех групп выбранного факультета
     def all_groups(self) -> str:
@@ -77,7 +80,7 @@ class ScheduleExtractor:
             raise Exception(error)
 
     # Запрос списка групп
-    def get_groups_list(self) -> list[dict[str, Any]]:
+    def get_groups_list(self) -> List[Dict[str, Any]]:
         url_group = self.all_groups()
         list_of_groups = self.http_request.send_request("GET", url_group)
         return list_of_groups
@@ -93,31 +96,50 @@ class ScheduleExtractor:
             raise Exception(error)
 
     # Запрос расписания для конкретной группы на сегодня
-    def get_schedule(self) -> list[dict[str, Any]]:
+    def request_schedule(self) -> List[Dict[str, Any]]:
         params = {
             "id": self.get_group_id(),
             "dateFrom": self.date_obj.date_from(),
             "dateTo": self.date_obj.date_to()
         }
-        schedule_request = self.http_request.send_request("GET", URL_SCHEDULE, params=params)
-        return schedule_request
+        self.schedule_list = self.http_request.send_request("GET", URL_SCHEDULE, params=params)
+        self.form_pure_schedule_list()
+        return self.schedule_list
+
+    # Собрать все занятия по дням, по которым стоит отправлять уведомление
+    def form_pure_schedule_list(self):
+        self.schedule_list
+        for day in self.schedule_list:
+            self.pure_schedule_list[day.get("date")] = dict()
+            for lessons in day.get("lessons"):
+                if lessons.get("type") == "LESSON":
+
+                    time = dt.fromtimestamp(lessons.get("starts")).time()
+                    self.pure_schedule_list[day.get("date")][lessons.get("title")] = dict()
+                    self.pure_schedule_list[day.get("date")][lessons.get("title")]["starts"] = dt.fromtimestamp(lessons.get("starts")).time()
+                    self.pure_schedule_list[day.get("date")][lessons.get("title")]["ends"] = dt.fromtimestamp(lessons.get("ends")).time()
+                    self.pure_schedule_list[day.get("date")][lessons.get("title")]["format"] = lessons.get("audience").get("name")
 
     # print расписания
     def print_schedule(self):
-        print(self.get_schedule())
+        print(self.schedule_list)
 
     # Сохранение в JSON
     def save_to_json(self):
-        schedule_list = self.get_schedule()
+        self.request_schedule()
         faculty_label_list = self.faculty.split()
         faculty_translited = translit("_".join(faculty_label_list), language_code="ru", reversed=True)
+        pure_faculty_translited = "pure_" + faculty_translited
         with open(f"{faculty_translited}_{self.group_name}.json", "w") as file:
-            json.dump(schedule_list, file)
+            json.dump(self.schedule_list, file, ensure_ascii=False)
+        with open(f"{pure_faculty_translited}_{self.group_name}.json", "w") as file:
+            json.dump(self.pure_schedule_list, file, ensure_ascii=False, default=str)
 
 
 if __name__ == '__main__':
     current_faculty = "Институт прикладной математики и компьютерных наук"
     current_group = "932209"
-    selected_day = "01.05.2023"
+    selected_day = "15.05.2023"
     schedule = ScheduleExtractor(current_faculty, current_group, selected_day)
     schedule.save_to_json()
+    t = 0
