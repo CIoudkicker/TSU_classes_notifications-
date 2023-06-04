a5aba0eb8facd54cfcac7c14c9e8e0ed1f50af05
Lieeoo <lizazenkina@ya.ru>
2023-06-04 15:14:28 +0700

    Наметки генерации данных на странице после их получения, очень ранние наметки

--- tsu-schedule-client/src/components/scheduleData.js
+++ tsu-schedule-client/src/components/scheduleData.js
@@ -2,15 +2,15 @@ const scheduleData = [
     {
       day: 'Monday',
       lessons: [
-        { startTime: '9:00 AM', endTime: '11:00 AM', type: 'lecture', title: 'Introduction to React' },
-        { startTime: '11:00 AM', endTime: '12:00 PM', type: 'practice', title: 'Building a Todo App' },
+        { startTime: '8:45', endTime: '10:20', type: 'lecture', title: 'Introduction to React' },
+        { startTime: '10:35', endTime: '12:10', type: 'practice', title: 'Building a Todo App' },
       ],
     },
     {
       day: 'Tuesday',
       lessons: [
-        { startTime: '9:00 AM', endTime: '11:00 PM', type: 'lecture', title: 'React Hooks' },
-        { startTime: '11:00 AM', endTime: '12:00 PM', type: 'meeting', title: 'Team Meeting' },
+        { startTime: '8:45', endTime: '10:20', type: 'lecture', title: 'React Hooks' },
+        { startTime: '10:35', endTime: '12:10', type: 'meeting', title: 'Team Meeting' },
       ],
     },
     // ...and so on for the rest of the week
--- tsu-schedule-client/src/components/scheduleTable.js
+++ tsu-schedule-client/src/components/scheduleTable.js
@@ -4,6 +4,24 @@ import { Container, Row, Col, Table, Button, Modal } from 'react-bootstrap';
 import scheduleData from './scheduleData';
 import { useNavigate } from 'react-router';
 
+let scheduleDataThis = [
+    {
+      day: 'Monday',
+      lessons: [
+        { startTime: '8:45', endTime: '10:20', type: 'lecture', title: 'Introduction to React' },
+        { startTime: '10:35', endTime: '12:10', type: 'practice', title: 'Building a Todo App' },
+      ],
+    },
+    {
+      day: 'Tuesday',
+      lessons: [
+        { startTime: '8:45', endTime: '10:20', type: 'lecture', title: 'React Hooks' },
+        { startTime: '10:35', endTime: '12:10', type: 'meeting', title: 'Team Meeting' },
+      ],
+    },
+    // ...and so on for the rest of the week
+  ];
+
 const ScheduleTable = () => {
   const navigate = useNavigate();
   // Хук для отоборажения окошка с дополнительной информацией о паре
@@ -23,8 +41,13 @@ const ScheduleTable = () => {
 
   // Время начала и конца пар
   const timeMarks = [
-    { startTime: '9:00 AM', endTime: '11:00 AM' },
-    { startTime: '11:00 AM', endTime: '12:00 PM' }
+    { startTime: '8:45', endTime: '10:20' },
+    { startTime: '10:35', endTime: '12:10' },
+	{ startTime: '12:25', endTime: '14:00' },
+	{ startTime: '14:45', endTime: '16:20' },
+	{ startTime: '16:35', endTime: '18:10' },
+	{ startTime: '18:25', endTime: '20:00' },
+	{ startTime: '20:15', endTime: '21:50' }
   ];
 
   // Цвет пары в таблице
@@ -33,7 +56,10 @@ const ScheduleTable = () => {
     practice: 'primary',
     meeting: 'warning'
   };
-
+  
+  window.onload = function() {
+			alert('1');
+		};
   return (
     <Container>
 	  <Row>
@@ -65,7 +91,7 @@ const ScheduleTable = () => {
               {timeMarks.map((timeMark) => (
                 <tr key={timeMark.startTime}>
                   <td>{`${timeMark.startTime} - ${timeMark.endTime}`}</td>
-                  {scheduleData.map((day) => {
+                  {scheduleDataThis.map((day) => {
                     const lesson = day.lessons.find((l) => l.startTime === timeMark.startTime);
                     return (
                       <td key={day.day + timeMark.startTime}>
@@ -99,5 +125,9 @@ const ScheduleTable = () => {
     </Container>
   );
 };
+
+async function fillScedule (){
+	
+}
 
 export default ScheduleTable;
